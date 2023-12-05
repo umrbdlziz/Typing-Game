a913956c5e0f39140e3bef16d3a4b5342c340363
umxziz <umxziz@student.usm.my>
2023-12-05 13:30:13 +0800

    Add text
    add text for typing

---
+++ typing.js
@@ -0,0 +1,25 @@
+const words =
+  "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum".split(
+    " "
+  );
+const wordsCount = words.length;
+
+function randomWord() {
+  const randomIndex = Math.ceil(Math.random() * wordsCount);
+  return words[randomIndex];
+}
+
+function formatWord(word) {
+  return `<div class="word"><span>${word
+    .split("")
+    .join('</span><span class="letter">')}</span></div>`;
+}
+
+function newGame() {
+  document.getElementById("words").innerHTML = "";
+  for (let i = 0; i < 200; i++) {
+    document.getElementById("words").innerHTML += formatWord(randomWord());
+  }
+}
+
+newGame();
